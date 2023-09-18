0e7a1ad7c3dded2d18b7f00a21611801947cd315
Albert Hua <ashua2004@gmail.com>
2023-09-18 16:10:08 -0400

    first commit (debug later)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { SessionProvider } from "next-auth/react";
+
+import Layout from "@/Components/Layout";
+import LoginModal from "@/Components/modals/LoginModal";
+import RegisterModal from "@/Components/modals/RegisterModal";
+import "@/styles/globals.css";
+import EditModal from "@/Components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <EditModal />
+      <RegisterModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
---
+++ pages/posts/[postId].tsx
@@ -0,0 +1,36 @@
+import Form from "@/Components/Form";
+import Header from "@/Components/Header";
+import CommentFeed from "@/Components/posts/CommentFeed";
+import PostItem from "@/Components/posts/PostItem";
+import usePost from "@/hooks/usePost";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const PostView = () => {
+  const router = useRouter();
+  const { postId } = router.query;
+
+  const { data: fetchedPost, isLoading } = usePost(postId as string);
+
+  if (isLoading || !fetchedPost) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
+  return (
+    <>
+      <Header label="Tweet" showBackArrow />
+      <PostItem data={fetchedPost} />
+      <Form
+        postId={postId as string}
+        isComment
+        placeholder="Tweet your reply"
+      />
+      <CommentFeed comments={fetchedPost?.comments} />
+    </>
+  );
+};
+
+export default PostView;
