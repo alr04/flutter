53fc95a73c64fe0a5054e33ee3b78903506179c1
Albert Hua <ashua2004@gmail.com>
2023-09-18 21:21:43 -0400

    updated tailwind.config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Form from "@/components/Form";
-import Header from "@/components/Header";
-import PostFeed from "@/components/posts/PostFeed";
+import Form from "@/Components/Form";
+import Header from "@/Components/Header";
+import PostFeed from "@/Components/posts/PostFeed";
 
 export default function Home() {
   return (
--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,7 +1,7 @@
-import Form from "@/components/Form";
-import Header from "@/components/Header";
-import CommentFeed from "@/components/posts/CommentFeed";
-import PostItem from "@/components/posts/PostItem";
+import Form from "@/Components/Form";
+import Header from "@/Components/Header";
+import CommentFeed from "@/Components/posts/CommentFeed";
+import PostItem from "@/Components/posts/PostItem";
 import usePost from "@/hooks/usePost";
 import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
